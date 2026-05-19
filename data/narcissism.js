3b53202b1a1a1b4e6b4afffe214818684dee8b44
moonelily33 <moonelily33@gmail.com>
2026-05-19 12:21:24 +0700

    Create narcissism.js

---
+++ data/narcissism.js
@@ -0,0 +1,62 @@
+window.NARCISSISM = [
+  {
+    name: "Jeno Narcissism Album Photocard",
+    image: "image/jeno1.png",
+    member: "jeno",
+    category: "photocard",
+    type: "album",
+    typeLabel: "Album",
+    era: "narcissism",
+    eraName: "Narcissism"
+  },
+  {
+    name: "Jaemin Narcissism POB",
+    image: "image/jaemin1.png",
+    member: "jaemin",
+    category: "photocard",
+    type: "pre-order-benefit",
+    typeLabel: "POB",
+    era: "narcissism",
+    eraName: "Narcissism"
+  },
+  {
+    name: "Jeno Narcissism Lucky Draw",
+    image: "image/jeno1.png",
+    member: "jeno",
+    category: "photocard",
+    type: "lucky-draw",
+    typeLabel: "Lucky Draw",
+    era: "narcissism",
+    eraName: "Narcissism"
+  },
+  {
+    name: "Jaemin Narcissism Fansign",
+    image: "image/jaemin1.png",
+    member: "jaemin",
+    category: "photocard",
+    type: "fan-sign",
+    typeLabel: "Fan Sign",
+    era: "narcissism",
+    eraName: "Narcissism"
+  },
+  {
+    name: "Unit Narcissism Photocard Set",
+    image: "image/unit1.png",
+    member: "unit",
+    category: "photocard",
+    type: "etc",
+    typeLabel: "Etc",
+    era: "narcissism",
+    eraName: "Narcissism"
+  },
+  {
+    name: "Narcissism Merchandise",
+    image: "image/md1.png",
+    member: "other",
+    category: "non-pc",
+    type: "merchandise",
+    typeLabel: "Merchandise",
+    era: "narcissism",
+    eraName: "Narcissism"
+  }
+];
